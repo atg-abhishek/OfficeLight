85f359a2565ffde2a1fd25a1d8de26baa34e0fc6
atg-abhishek <atg.abhishek@gmail.com>
2015-06-06 17:37:09 -0400

    working backend

--- backend/OfficeLight.py
+++ backend/OfficeLight.py
@@ -15,6 +15,8 @@ import array
 import struct
 import matplotlib.pyplot as plt
 import time
+import pprint
+from datetime import datetime
 
 from mules_utils import mules_parse_header, mules_parse_data
 
@@ -34,64 +36,89 @@ if __name__ == "__main__":
     #conn, addr = client_cnx.accept()
     
     # 2 Request of Header
-
-   # for     
-    
-    print('Header request')
-    command = 'H'
-    client_cnx.send(command)
-    
-    nBytes_4B = array.array('B',client_cnx.recv(4) )
-    nBytes = struct.unpack('i',nBytes_4B[::-1])[0]
-    #nBytes equals to number of bytes to read from the connection
-    #[0] is used to get an int32 rather than turple
-    package = client_cnx.recv(nBytes)
-    #edit and obtain parameters from the package
-    dev_name, dev_hardware, fs, data_format, nCh = mules_parse_header(package)
-    
-    # 3 Request of channel names
-    print('Channel names request')
-    command = 'N'
-    client_cnx.send(command)
-    
-    nBytes_4B = array.array('B',client_cnx.recv(4)) 
-    nBytes = struct.unpack('i',nBytes_4B[::-1])[0]
-    package = client_cnx.recv(nBytes)
-    ch_labels = package.split(',')
-    
-    # 4 Flush old data from the Server data
-    print('Flushing Server data')
-    command = 'F'
-    client_cnx.send(command)
-
-    # 5 Pause script 5 seconds to gather data 
-    print('Wait 5 seconds');
-    time.sleep(15);
-    
-    # 6 Request EEG data since the last flush (5 seconds)
-    print('EEG data request');
-    command = 'R';
-    client_cnx.send(command)
-    nBytes_4B = array.array('B',client_cnx.recv(4)) 
-    nBytes = struct.unpack('i',nBytes_4B[::-1])[0]
-
-    eeg_package = '';
-    while len(eeg_package) < nBytes:
-        eeg_package += client_cnx.recv(1)
-    
-    eeg_data = mules_parse_data(eeg_package, data_format)
-    
-    # 7 Plot EEG data
-    n_samples = eeg_data.shape[0];
-    time_vec = np.arange(n_samples)/fs;
-    
-    plt.ion()    
-    plt.plot(time_vec,eeg_data); 
-    plt.xlabel('Time [s]');
-    plt.ylabel('Amplitude');
-    plt.show()
-    
+    i=0
+    j=0
+    result = []
+    measure_unit = "Amplitude"
+    loc_esi_id = "Muse-0CCD"
+    direction = "P"
+    print ('Use a keyboard interrupt to stop')
+    try:  
+        #needs to be True for infinite pinging 
+        #also remove all the other prints
+        #in the while loop let the condition be based on an hour from the current time and then 
+        #send all of the data in the end at once
+        while i<100:    
+            print('Header request')
+            command = 'H'
+            client_cnx.send(command)
+            
+            nBytes_4B = array.array('B',client_cnx.recv(4) )
+            nBytes = struct.unpack('i',nBytes_4B[::-1])[0]
+            #nBytes equals to number of bytes to read from the connection
+            #[0] is used to get an int32 rather than turple
+            package = client_cnx.recv(nBytes)
+            #edit and obtain parameters from the package
+            dev_name, dev_hardware, fs, data_format, nCh = mules_parse_header(package)
+            
+            # 3 Request of channel names
+            print('Channel names request')
+            command = 'N'
+            client_cnx.send(command)
+            
+            nBytes_4B = array.array('B',client_cnx.recv(4)) 
+            nBytes = struct.unpack('i',nBytes_4B[::-1])[0]
+            package = client_cnx.recv(nBytes)
+            cur_time_init = str(datetime.now())
+            ch_labels = package.split(',')
+            
+            # 4 Flush old data from the Server data
+            print('Flushing Server data')
+            command = 'F'
+            client_cnx.send(command)
         
-    # 8 Close connection with Server
-    client_cnx.close()
-    print('Connection closed.')
+            # 5 Pause script 5 seconds to gather data 
+#            print('Wait 5 seconds');
+            # getting data in 0.05 second intervals
+            time.sleep(0.05);
+            
+            # 6 Request EEG data since the last flush (5 seconds)
+            print('EEG data request');
+            command = 'R';
+            client_cnx.send(command)
+            nBytes_4B = array.array('B',client_cnx.recv(4)) 
+            nBytes = struct.unpack('i',nBytes_4B[::-1])[0]
+        
+            eeg_package = '';
+            while len(eeg_package) < nBytes:
+                eeg_package += client_cnx.recv(1)
+            
+            eeg_data = mules_parse_data(eeg_package, data_format)
+            
+            # 7 Plot EEG data
+            n_samples = eeg_data.shape[0];
+            if n_samples!=1:
+                time_vec = np.arange(n_samples)/fs
+                avg_value = time_vec.mean()
+                j=j+1
+                sub_result = {'measure unit' : measure_unit, 'loc_esi_id' : loc_esi_id, 'direction' : direction, 'value' : avg_value, 'tstamp' : cur_time_init}
+#                pprint.pprint(sub_result)                
+                result.append(sub_result) 
+#            print ('nsamples '+str(n_samples))
+            #pprint.pprint(time_vec)           
+#            plt.ion()    
+#            plt.plot(time_vec,eeg_data); 
+#            plt.xlabel('Time [s]');
+#            plt.ylabel('Amplitude');
+            #plt.show()
+#            print ('i '+str(i))
+            i=i+1
+            
+        
+#        print ("j value " + str(j))
+        pprint.pprint(result)
+    except KeyboardInterrupt:
+        
+        # 8 Close connection with Server
+        client_cnx.close()
+        print('Connection closed.')
